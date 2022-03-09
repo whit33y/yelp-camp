e069ad175d1dec71d4fcf3a011ea928044301814
whit33y <troppout@gmail.com>
2022-03-09 15:02:45 +0100

    Added login form and passport check

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const { append } = require('express/lib/response');
 const router = express.Router();
 const User = require('../models/user');
 const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport')
+const passportLocal = require('passport-local')
 
 router.get('/register', (req,res)=>{
     res.render('users/register')
@@ -18,6 +21,17 @@ router.post('/register', wrapAsync(async(req,res)=>{
             req.flash('error', e.message);
             res.redirect('register')
      }
-   
 }))
+
+//login
+router.get('/login', (req,res)=>{
+    res.render('users/login')
+})
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),(req,res)=>{
+    req.flash('success', 'Welcome back!')
+    res.redirect('/campgrounds')
+})
+
+
+
 module.exports = router;
