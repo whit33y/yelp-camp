3f108b7f00cc46ad2435b734c8a1ac6679be4fd5
whit33y <troppout@gmail.com>
2022-03-11 11:22:59 +0100

    Finishing touches to authorization, it now stays logged in after registration and return to pages that we were trying to reach

--- routes/users.js
+++ routes/users.js
@@ -14,9 +14,15 @@ router.post('/register', wrapAsync(async(req,res)=>{
     const {email, username, password} = req.body;
     const user = new User({email, username});
     const registeredUser = await User.register(user, password);
+    req.login(registeredUser, err =>{
+        if(err){
+            return next(err)
+        }
     console.log(registeredUser)
     req.flash('success','Welcome to Yelp Camp!')
     res.redirect('/campgrounds')
+    })
+    
      }catch(e){
             req.flash('error', e.message);
             res.redirect('register')
@@ -29,7 +35,9 @@ router.get('/login', (req,res)=>{
 })
 router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}),(req,res)=>{
     req.flash('success', 'Welcome back!')
-    res.redirect('/campgrounds')
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.redirectUrl
+    res.redirect(redirectUrl)
 })
 //logout
 router.get('/logout', (req,res)=>{
